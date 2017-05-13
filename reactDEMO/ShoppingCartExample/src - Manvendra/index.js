5a0e6f52e99f8a144d04400761368168ef8eedd4
Manvendra Shaktawat <mshakt@sapient.com>
2017-05-13 17:12:43 +0530

    Repo cleaned

--- reactDEMO/ShoppingCartExample/src - Manvendra/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import ShoppingCart from "./ShoppingCart";
-
-ReactDOM.render(
-	<ShoppingCart header="SapientNitro Watch Shop"/>,
-	document.getElementById('root')
-);
--- reactDEMO/ShoppingCartExample/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import DesignerNews from "./DesignerNews";
-
-ReactDOM.render(
-	<DesignerNews/>,
-	document.getElementById('root')
-);
